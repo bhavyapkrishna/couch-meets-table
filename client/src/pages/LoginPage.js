05d66484b3f346554e2cb73f656fc5c92446cafb
giahur <hurgia21@gmail.com>
2025-02-26 02:46:37 -0500

    password and email auth basics

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,32 +1,56 @@
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const LoginPage = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleLogin = () =>
     {
+        if(!email.endsWith("@case.edu")) {
+            alert("Please enter a valid CWRU email address")
+            return;
+        }
         navigate("/");
     };
 
+    const setPasswordVisible = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div className = "container">
-            <h1>Log In</h1>
-            <input 
-                type="text" 
-                placeholder="CWRU Email" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input 
-                type="password" 
-                placeholder="Password" 
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin} className="custom-btn">Log In</button>
+       <div className="container-fluid custom-bg vh-100 d-flex flex-column justify-content-start pt-5">
+            <Navbar />
+            {/*Content*/}
+            <div className="card p-4 text-center mt-5 border-0 custom-bg">
+                <h1 className="h3 custom-txt">Log In</h1>
+                <input 
+                    type="text" 
+                    placeholder="CWRU Email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <span
+                    onMouseDown={setPasswordVisible}
+                    onMouseUp={setPasswordVisible}
+                    onMouseLeave={() => setShowPassword(false)}
+                >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </span>
+            </div>
+            <div className="mt-3">
+                <button onClick={handleLogin} className="custom-btn">Log In</button>
+            </div>
         </div>
     )
 }
