9fe56726b94c579eca6a613c2066c27d8e47c7ec
giahur <hurgia21@gmail.com>
2025-02-26 02:24:23 -0500

    reusable nav and some login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,13 +1,34 @@
-import {useNavigate, userNavigate} from "react-router-dom";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 
-function LoginPage()
-{
+const LoginPage = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleLogin = () =>
     {
-        navigate("/home");
-    }
+        navigate("/");
+    };
+
+    return (
+        <div className = "container">
+            <h1>Log In</h1>
+            <input 
+                type="text" 
+                placeholder="CWRU Email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin} className="custom-btn">Log In</button>
+        </div>
+    )
 }
 
 export default LoginPage;
