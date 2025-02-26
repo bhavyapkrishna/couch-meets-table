fd03c1262caa891bb7eaa31821ed68d6d41e840b
Sabertooth141 <44132181+Sabertooth141@users.noreply.github.com>
2025-02-26 03:24:06 -0500

    wip

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,9 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function HomePage()
 {
     return (
-        <div className="container-fluid bg-light vh-100 d-flex flex-column justify-content-start pt-5">
+        <div className="container-fluid custom-bg vh-100 d-flex flex-column justify-content-start pt-5">
             {/*Navbar*/}
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top py-3">
+            <nav className="navbar navbar-expand-lg navbar-dark custom-sec-bg fixed-top py-3">
                 <div className="container-fluid">
                     <span className="navbar-brand ms-3">Couch Meets Table</span>
                     <span className="ms-auto me-3">
@@ -18,15 +18,15 @@ export default function HomePage()
             </nav>
 
             {/*Content*/}
-            <div className="card p-4 text-center shadow mt-5">
-                <h1 className="h3 text-primary">Are you ready to find your perfect roommate?</h1>
-                <p className="text-muted">Just fill out this quick questionnaire!</p>
+            <div className="card p-4 text-center mt-5 border-0 custom-bg">
+                <h1 className="h3 custom-txt">Are you ready to find your perfect roommate?</h1>
+                <p className="custom-txt">Just fill out this quick questionnaire!</p>
             </div>
 
             {/*Start button*/}
             <div className="mt-3">
                 <Link to="/login">
-                    <button className="btn btn-primary">Start</button>
+                    <button className="custom-btn">Start</button>
                 </Link>
             </div>
         </div>
