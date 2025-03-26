bb078ad3a7e56ec5b74363ab188f8f7a3f02d8db
emmachio <chio@STEPHENs-MacBook-Pro-9.local>
2025-03-25 22:04:55 -0400

    compatability test trial

--- server/compatabilityTest.py
+++ server/compatabilityTest.py
@@ -3,6 +3,7 @@ class Person:
     features = []
 
     # initializing the person with their trait preference on a 1-5 scale
+    # wake up time, sleep time, quietness, cleaniness, guest, in room?
     def __init__(self, wu, st, q, c, g, b):
         self.features = []
         self.features.append(wu)
@@ -12,16 +13,18 @@ class Person:
         self.features.append(g)
         self.features.append(b)
         self.numberOfFeatures = len(self.features)
+
+    # function that would determine the person's compatability with someone else's
     def determineCompatability(self, person2):
         currentCompatability = 0
         for i in range(self.numberOfFeatures):
             difference = abs(self.features[i] - person2.features[i])
-            currentCompatability = currentCompatability + (difference/5) * (1/self.numberOfFeatures)
+            currentCompatability = currentCompatability + (difference/4) * (1/self.numberOfFeatures)
         currentCompatability = (1 - currentCompatability)*100
         return(currentCompatability)
 
-person1 = Person(3, 2, 5, 3, 1, 0)
-person2 = Person(4, 2, 5, 3, 1, 0)
+person1 = Person(3, 2, 5, 3, 1, 1)
+person2 = Person(4, 2, 5, 3, 1, 1)
 
 print(person1.determineCompatability(person2))
 
