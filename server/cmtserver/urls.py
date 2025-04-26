ae60dc6617add70016c165b1043a8d48c4ef380f
bhavya <110128384+bhavyapkrishna@users.noreply.github.com>
2025-04-26 11:25:00 -0400

    logout functionality backend

--- server/cmtserver/urls.py
+++ server/cmtserver/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, get_current_user_profile
+from .views import RegisterView, get_current_user_profile, logout
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', get_current_user_profile, name='get_profile'),
+    path('logout/', logout, name='logout')
 ]
