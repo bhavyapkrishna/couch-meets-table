be36e6a415524d69c061842fbddc5675dea5af43
emmachio <emmany2409@gmail.com>
2025-04-26 11:19:43 -0400

    updated threshold

--- server/cmtserver/utils.py
+++ server/cmtserver/utils.py
@@ -8,7 +8,7 @@ def determineCompatability(person1, person2, person1Impo):
         # if it is important, then add to the current compatibility if it is the same value
         if person1Impo[i] == 1:
             # if difference = 4, 4/4 = 1 => similar
-            if difference == 4:
+            if difference >= 3:
                 currentCompatability = currentCompatability + (difference/4)
             # if it is not the same value, set it to infinity
             else:
